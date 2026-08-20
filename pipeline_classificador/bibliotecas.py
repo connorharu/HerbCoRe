f215a9f133aad43bd23282014ff99a4f23668571
= <harumicollegestuff@gmail.com>
2026-08-20 00:37:34 -0300

    adicao do treinamento do classificador versao pipeline

---
+++ pipeline_classificador/bibliotecas.py
@@ -0,0 +1,15 @@
+import argparse
+import random
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import glob
+import os
+import shutil
+import pathlib
+import tensorflow as tf
