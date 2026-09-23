fc837c0b446a829868b1c376520f734907b37621
= <harumicollegestuff@gmail.com>
2026-09-22 23:32:05 -0300

    pipeline completely translated

--- pipeline/library.py
+++ pipeline/library.py
@@ -10,25 +10,25 @@ import os
 import subprocess
 import shutil
 
-diretorio = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(diretorio)
-ferramenta_path = os.path.join(parent_dir, 'herbcore_tool')
-if ferramenta_path not in sys.path:
-    sys.path.append(ferramenta_path)
+directory = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(directory)
+tool_path = os.path.join(parent_dir, 'herbcore_tool')
+if tool_path not in sys.path:
+    sys.path.append(tool_path)
 
 from config import get_config, ask_for_missing_values
 from main_f import species_link
-from sinonimos import extract_plants_from_txt, save_plants_to_csv, perform_lcvp_fuzzy_search, perform_lcvp_fuzzy_search_per_line, save_result_to_txt_aligned
+from synonym import extract_plants_from_txt, save_plants_to_csv, perform_lcvp_fuzzy_search, perform_lcvp_fuzzy_search_per_line, save_result_to_txt_aligned
 from rpy2.robjects.packages import importr
-from deduplicacao import processar
+from deduplicating import processing
 
 __all__ = [
     'json', 'os', 'sys', 'mysql_conn', 'plt', 'pd', 'tempfile', 
-    'subprocess', 'StringIO', 'configurar', 'species_link',
-    'extract_plants_from_txt', 'processar', 'perform_lcvp_fuzzy_search_per_line', 'shutil'
+    'subprocess', 'StringIO', 'configure', 'species_link',
+    'extract_plants_from_txt', 'processing', 'perform_lcvp_fuzzy_search_per_line', 'shutil'
 ]
 
-def configurar():
+def configure():
     config = get_config()
 
     if config is None or any(k not in config for k in ['api_key', 'db_user', 'db_password', 'db_host', 'db_schema']):
